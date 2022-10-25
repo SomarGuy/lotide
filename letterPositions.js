0d1a6891af2774c84f078ce9d784c89f8f7b7453
Somaro <106792137+SomarGuy@users.noreply.github.com>
2022-10-25 12:12:09 -0400

    added eqArrays & assertEqual to test code

---
+++ letterPositions.js
@@ -0,0 +1,19 @@
+const assertEqual = function(actual, expected) {
+  if (actual !== expected) (
+    console.log(`🛑🛑🛑Assertion Failed ${actual} !== ${expected}`)
+  ); else (actual === expected) (
+    console.log(`✅✅✅Assertion Passed ${actual} === ${expected}`)
+  );
+};
+
+const eqArrays = function(arrOne, arrTwo) {
+  if (arrOne.length !== arrTwo.length) {
+    return false;
+  }
+  for (let i = 0; i < arrOne.length; i++) {
+    if (arrOne[i] !== arrTwo[i]) {
+      return false;
+    }
+  }
+  return true;
+}
