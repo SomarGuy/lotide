16c91abb1404c7bfe1a15c6f650c15e8b5929317
Somaro <106792137+SomarGuy@users.noreply.github.com>
2022-10-25 17:22:19 -0400

    eqObjects Updated

--- eqObjects.js
+++ eqObjects.js
@@ -10,5 +10,11 @@ const eqObjects = function(objOne, objTwo) {
   if (Object.keys(objOne).length !== Object.keys(objTwo).length) {
     return false;
   }
-  
+  for (const key of Object.keys(objOne)) {
+    if (Array.isArray(objOne[key]) && Array.isArray(objTwo[key])) {
+      if (!eqArrays(objOne[key], objTwo[key])) {
+        return false;
+      }
+    }
+  }
 }
