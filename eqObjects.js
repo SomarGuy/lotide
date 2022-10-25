0cab52b3561c994f788b7bb77bc1174255b22c4d
Somaro <106792137+SomarGuy@users.noreply.github.com>
2022-10-25 17:44:40 -0400

    Updated eqObjects

--- eqObjects.js
+++ eqObjects.js
@@ -6,6 +6,18 @@ const assertEqual = function(actual, expected) {
   );
 };
 
+const eqArrays = function(arrOne, arrTwo) {
+  if (arrOne.length !== arrTwo.length) {
+    return false;
+  }
+  for (let i = 0; i < arrOne.length; i++) {
+    if (arrOne[i] !== arrTwo[i]) {
+      return false;
+    }
+  }
+  return true;
+}
+
 const eqObjects = function(objOne, objTwo) {
   if (Object.keys(objOne).length !== Object.keys(objTwo).length) {
     return false;
@@ -16,5 +28,28 @@ const eqObjects = function(objOne, objTwo) {
         return false;
       }
     }
+    if (objOne[key] instanceof Object && objTwo[key] instanceof Object) {
+      if (!eqObjects(objOne[key], objTwo[key])) {
+        return false;
+      }
+    } else {
+      if (objOne[key] !== objTwo[key]) {
+        return false;
+      }
+    }
   }
+  return true;
 }
+
+const ab = { a: '1', b: '2'};
+const ba = { b: '2', a: '1'};
+const abc = { a: '1', b: '2', c: '3'};
+assertEqual(eqObjects(ab, ba), true);
+assertEqual(eqObjects(ab, abc), false);
+
+const cd = { c: '1', d: ['2', 3] };
+const dc = { d: ['2', 3], c: '1'};
+assertEqual(eqObjects(cd, dc), true);
+
+const cd2 = { c: '1', d: ['2', 3, 4] };
+assertEqual(eqObjects(cd, cd2), false);
