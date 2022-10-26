247456a2fe22ed2b3a14b4050ebd4ce21c5bcb12
Somaro <106792137+SomarGuy@users.noreply.github.com>
2022-10-26 16:58:10 -0400

    Will take in two objects and console.log an appropriate message to the console.

--- assertObjectsEqual.js
+++ assertObjectsEqual.js
@@ -32,3 +32,20 @@ const eqArrays = function(arrOne, arrTwo) {
   }
   return true;
 }
+
+const assertObjectsEqual = function(actual, expected) {
+  const inspect = require("util").inspect;
+  if (eqObjects(actual, expected)) {
+    console.log(`✅✅✅ Assertion Passed: ${inspect(actual)} === ${inspect(expected)}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${inspect(actual)} !== ${inspect(expected)}`);
+  }
+};
+
+const ab = { a: '1', b: '2'};
+const ba = { b: '2', a: '1'};
+const abc = { a: '1', b: '2', c: '3'};
+
+assertObjectsEqual(ab, ba); //Passes
+assertObjectsEqual(ab, abc); //Fail
+assertObjectsEqual(ba, abc); //Fail
