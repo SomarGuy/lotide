d7ed7c3596e031c9fa6ade6e7e60662db1946543
Somaro <106792137+SomarGuy@users.noreply.github.com>
2022-11-11 15:50:40 -0500

    Refactored tail.js

--- tail.js
+++ tail.js
@@ -1,20 +1,5 @@
-const assertEqual = function(actual, expected) {
-  if (actual !== expected) (
-    console.log(`🛑🛑🛑Assertion Failed ${actual} !== ${expected}`)
-  ); else if (actual === expected) (
-    console.log(`✅✅✅Assertion Passed ${actual} === ${expected}`)
-  );
-};
-
 const tail = function(arr) {
-  return(arr.slice(1));
+  return arr.slice(1);
 };
 
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words); // no need to capture the return value since we are not checking it
-assertEqual(words.length, 3); // original array should still have 3 elements!
-const result = tail(["Hello", "Lighthouse", "Labs"]);
-assertEqual(result, ["Lighthouse", "Labs"]); // => will always fail!
-assertEqual(words.length-1, tail(words).length);
-const resultTwo = tail(words);
-assertEqual(words[1],resultTwo[0]);
+module.exports = tail;
