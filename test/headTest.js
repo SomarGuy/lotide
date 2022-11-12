0eaa5caea000f3a54ef349fdaacf3f8c2dfb894e
Somaro <106792137+SomarGuy@users.noreply.github.com>
2022-11-12 09:49:22 -0500

    Refactored using mocha and chai

--- test/headTest.js
+++ test/headTest.js
@@ -1,9 +1,23 @@
 const assertEqual = require('../assertEqual');
 const head = require('../head');
+const assert = require('chai').assert;
 
+describe('#head', () => {
+  it("returns 1 for [1, 2, 3]", () => {
+    assert.strictEqual(head([1,2,3]), 1)
+  });
+});
+
+describe('#head', () => {
+  it("returns '5' for ['5']", () => {
+    assert.strictEqual(head(['5']), '5'); 
+  });
+});
+
+/*
 assertEqual(head([5,6,7]), 5);
 assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
 assertEqual(head(["LHL", "Lighthouse", "Labs"]), "Hello");
 assertEqual(head([7,8,9]), 5);
-
+*/
 
