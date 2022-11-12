e750287fec661e845e3267ab3ebeee782bf63726
Somaro <106792137+SomarGuy@users.noreply.github.com>
2022-11-12 11:00:13 -0500

    Refactored to use mocha and chai as testing

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,6 +1,19 @@
 const middle = require('../middle');
 const assertArraysEqual = require('../assertArraysEqual');
+const assert = require('chai').assert;
 
+describe('#middle', () => {
+  it("returns [] for [1, 2] & []", () => {
+    assert.deepEqual(middle([1, 2]), []);
+  });
+
+  it('Returns one middle element for an array with an odd number of elements', () => {
+    assert.deepEqual(middle([1, 2, 3, 4, 5]), [3]);
+  });
+});
+
+/*
 assertArraysEqual(middle([1,2]), []);
 assertArraysEqual(middle([1,2,3,4,5]), [3]);
 assertArraysEqual(middle([1,2,3,4]), [4]);
+*/
