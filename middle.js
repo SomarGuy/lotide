f5d4ce50cd33020d3fda5705aa7a9627aca8333c
Somaro <106792137+SomarGuy@users.noreply.github.com>
2022-11-12 09:37:40 -0500

    Refactored middleJS

--- middle.js
+++ middle.js
@@ -1,24 +1,3 @@
-const eqArrays = function(arrOne, arrTwo) {
-  if (arrOne.length !== arrTwo.length) {
-    return false;
-  }
-  for (let i = 0; i < arrOne.length; i++) {
-    if (arrOne[i] !== arrTwo[i]) {
-      return false;
-    }
-  }
-  return true;
-};
-
-const assertArraysEqual = function(arrOne, arrTwo) {
-  if (eqArrays(arrOne, arrTwo)) {
-    console.log(`🛑🛑🛑: These arrays are not equal: ${arrOne} !== ${arrTwo}`);
-  } else {
-    console.log(`✅✅✅: These arrays are equal; ${arrOne} === ${arrTwo}`);
-  }
-};
-
-
 const middle = function(arr) {
   let newArr = [];
   if (arr.length === 1 || arr.length === 2) {
@@ -44,3 +23,5 @@ const middle = function(arr) {
     return newArr;
   }
 };
+
+module.exports = middle;
