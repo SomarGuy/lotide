6f5088810be98bc803395ce1dda541bffca2b092
Somaro <106792137+SomarGuy@users.noreply.github.com>
2022-11-12 11:25:52 -0500

    index for exporting all functions

---
+++ index.js
@@ -0,0 +1,38 @@
+const assertArraysEqual = require('./assertArraysEqual');
+const assertEqual = require('./assertEqual');
+const assertObjectsEqual = require('./assertObjectsEqual');
+const countLetters = require('./countLetters');
+const countOnly = require('./countOnly');
+const eqArrays = require('./eqArrays');
+const findKey = require('./findKey');
+const findKeyByValue = require('./findKeyByValue');
+const flatten = require('./flatten');
+const head   = require('./head');
+const letterPositions = require('./letterPositions');
+const map = require('./map');
+const takeUntil = require('./takeUntil');
+const without = require('./without');
+const tail   = require('./tail');
+const middle = require('./middle');
+const { count } = require('console');
+
+
+module.exports = {
+  assertArraysEqual,
+  assertEqual,
+  assertObjectsEqual,
+  countLetters,
+  countOnly,
+  eqArrays,
+  eqObjects,
+  findKey,
+  findKeyByValue,
+  flatten,
+  head,
+  letterPositions,
+  map,
+  takeUntil,
+  without,
+  tail,
+  middle
+};
