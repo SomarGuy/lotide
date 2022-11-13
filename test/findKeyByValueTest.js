cf8efb698fd379d4afa024c3ce22fda22dba5897
Somaro <106792137+SomarGuy@users.noreply.github.com>
2022-11-12 19:28:49 -0500

    Refactored using Mocha & Chai

--- test/findKeyByValueTest.js
+++ test/findKeyByValueTest.js
@@ -1,21 +1,15 @@
-const assert = require("chai").assert;
-const findKeyByValue = require("../findKeyByValue");
+const findKeyByValue = require('../findKeyByValue');
+const assert = require('chai').assert;
 
-describe("#findKeyByValue", () => {
-  const bestTVShowsByGenre = {
-    sci_fi: "The Expanse",
-    comedy: "Brooklyn Nine-Nine",
-    drama: "The Wire",
-  };
-
-  it("returns 'drama' for 'The Wire'", () => {
-    assert.strictEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
-  });
-
-  it("returns undefined if no movie matches", () => {
-    assert.strictEqual(
-      findKeyByValue(bestTVShowsByGenre, "That '70s Show"),
-      undefined
-    );
+describe("#find key by value", () => {
+  it("returns drama for The Wire", () => {
+    const bestTVShowsByGenre = {
+      "sci_fi": "The Expanse",
+      comedy: "Brooklyn Nine-Nine",
+      drama:  "The Wire"
+    };
+    const searchKey = "The Wire";
+    const expected = "drama";
+    assert.equal(findKeyByValue(bestTVShowsByGenre, searchKey), expected);
   });
 });
